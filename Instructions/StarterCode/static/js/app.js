eda04558059e6c2d0d734df42b8078ad65185c51
pfeliciano1 <79662467+pfeliciano1@users.noreply.github.com>
2021-07-15 21:54:20 -0400

    Defining Charts function for bar and bubble charts

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -41,7 +41,7 @@ function buildMetaData(sample) {
     // Clear the existing data in the html
     sampleMetadata.html("");
         // Specify the location of the metadata and update it
-    // Use `Object.entries` to add each key and value pair to the panelData
+    // Use `Object.entries` to add each key and value pair to the 'sampleMetadata' (or do a for loop)
     Object.entries(response).forEach(([key, value]) => {
         sampleMetadata.append("h6").text(`${key.toUpperCase()}: ${value}`);
         });
@@ -52,13 +52,68 @@ function buildMetaData(sample) {
 function buildCharts(sample) {
 
     // Read the json data
-    d3.json("samples.json").then((data) => {});
+    d3.json("samples.json").then((data) => {
+        
         // Parse and filter the data to get the sample's OTU data
-
+        let sampleData = data.samples;
+        let buildArray = sampleData .filter(sampleItem => sampleItem.id == sample);
+        let response = buildArray[0];
         // Pay attention to what data is required for each chart
-
+        let otu_ids = response.otu_ids;
+        let otu_labels = response.otu_labels;
+        let sample_values = response.sample_values;
         // Create bar chart in correct location
+        //Create a horizontal bar chart
+        let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        let trace1 = [
+        {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
+        }
+        ];
 
+        let layout1 = {
+        margin: { t: 30, l: 150 }
+        };
+
+        Plotly.newPlot("bar", trace1, layout1);
         // Create bubble chart in correct location
-    
+        // Build a Bubble Chart
+        let trace2 = [
+            {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+            }
+        ];
+        let layout2 = {
+            title: "Bacteria Cultures Per Sample",
+            hovermode: "closest",
+            xaxis: { title: "OTU ID" },
+        };
+
+        Plotly.newPlot("bubble", trace2, layout2);
+
+    });
 }
+
+function optionChanged(sample){
+    // The parameter being passed in this function is new sample id from dropdown menu
+
+    // Update metadata with newly selected sample
+    buildMetaData(newSample);
+    // Update charts with newly selected sample
+    buildCharts(newSample);
+}
+
+// Initialize dashboard on page load
+// init()
