c0d486c4bfa60c19101187b64f63bec66449a301
pfeliciano1 <79662467+pfeliciano1@users.noreply.github.com>
2021-07-15 21:17:26 -0400

    Defining Metadata function for sample

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -2,45 +2,59 @@
 This is not the only way to complete this assignment.
 Feel free to disregard and create your own code */
 
-// Define function that will run on page load
-function init() {
+// // Define function that will run on page load
+// function init() {
 
-    // Read json data
-    d3.json("samples.json").then(function(data) {
-        console.log(data);
+//     // Read json data
+//     d3.json("samples.json").then(function(data) {
+//         console.log(data);
       
-        // Parse and filter data to get sample names
-        let names = d3.select("names");
-        // Add dropdown option for each sample
-        // Use D3 to select the dropdown menu
-        let dropdownMenu = d3.select("#selDataset");
+//         // Parse and filter data to get sample names
+//         let names = d3.select("names");
+//         // Add dropdown option for each sample
+//         // Use D3 to select the dropdown menu
+//         let dropdownMenu = d3.select("#selDataset");
         
-    // Call functions below using the first sample to build metadata and initial plots
-        data.metadata.forEach(item =>
-            {
-            // console.log(item.id);
-            d3.select ("#selDataset").append('option').attr('value', item.id).text(item.id);
-            });
-    });
-}
+//     // Call functions below using the first sample to build metadata and initial plots
+//         data.metadata.forEach(item =>
+//             {
+//             // console.log(item.id);
+//             d3.select ("#selDataset").append('option').attr('value', item.id).text(item.id);
+//             });
+//     });
+// }
 
 // Define a function that will create metadata for given sample
-function buildMetadata(sample) {
+function buildMetaData(sample) {
 
     // Read the json data
-
+    d3.json("samples.json").then((data) => {
+        let metadata = data.metadata;
+        console.log(metadata);
+    
         // Parse and filter the data to get the sample's metadata
+    let buildArray = metadata.filter(sampleItem => sampleItem.id == sample);
+    let response = buildArray[0];
+    // Use d3 to select the required element on the page with the id property set to 'sample-metadata'
+    let sampleMetadata = d3.select("#sample-metadata");
 
+    // Clear the existing data in the html
+    sampleMetadata.html("");
         // Specify the location of the metadata and update it
-
+    // Use `Object.entries` to add each key and value pair to the panelData
+    Object.entries(response).forEach(([key, value]) => {
+        sampleMetadata.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
+    });
 }
 
 // Define a function that will create charts for given sample
 function buildCharts(sample) {
 
     // Read the json data
-
+    d3.json("samples.json").then((data) => {});
         // Parse and filter the data to get the sample's OTU data
+
         // Pay attention to what data is required for each chart
 
         // Create bar chart in correct location
@@ -48,17 +62,3 @@ function buildCharts(sample) {
         // Create bubble chart in correct location
     
 }
-
-
-function optionChanged(sample){
-    // The parameter being passed in this function is new sample id from dropdown menu
-
-    // Update metadata with newly selected sample
-
-    // Update charts with newly selected sample
-
-}
-
-// Initialize dashboard on page load
-init();
-
